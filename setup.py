14b7eae8fad6cfbced00e7af79e49af097984c2a
shruti <shrutiagr2806@gmail.com>
2022-11-27 16:15:00 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+REQUIREMENT_FILE = "requirements.txt"
+
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("/n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="ineuron",
+    author_email="avnish@ineuron.ai",
+    packages = find_packages(),
+    install_requires=get_requirements(),
+)
